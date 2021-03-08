c76e71f043bdc5182201cb987067b1621916785b
Samuel Breu <samuel.breu@bresam.ch>
2021-03-08 16:56:44 +0100

    travis

--- travis/script.sh
+++ travis/script.sh
@@ -3,7 +3,7 @@
 set -e
 
 DOCKER_BUILD=${DOCKER_BUILD-false}
-TRAVIS_PHP_VERSION=${TRAVIS_PHP_VERSION-7.0}
+TRAVIS_PHP_VERSION=${TRAVIS_PHP_VERSION-7.4}
 
 if [ "$DOCKER_BUILD" = false ]; then
     vendor/bin/phpunit --configuration phpunit.ci.xml --coverage-clover clover.xml
